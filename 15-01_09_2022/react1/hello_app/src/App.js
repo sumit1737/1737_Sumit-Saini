35141ab514cef6ca232c8be671dc5cfb713a1cb2
sumit1737 <sumit1737.be20@chitkara.edu.in>
2022-09-01 14:19:12 +0530

    1st component

---
+++ 15-01_09_2022/react1/hello_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>
+        Hello user
+      </h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ 15-01_09_2022/react1/hello_app/src/components/Header.js
@@ -0,0 +1,20 @@
+// Uppercase of Header.js's H is naming convention
+const Header = () => {
+    return (
+        <header>
+            <h1>Hello Header</h1>
+        </header>
+    )
+}
+
+// const Qwerty = () => {
+//     return (
+//         <header>
+//             <h1>Hello Header</h1>
+//         </header>
+//     )
+// }
+
+
+export default Header;
+// export default Qwerty; // ye nhi krne ka
